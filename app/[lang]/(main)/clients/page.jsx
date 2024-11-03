3e57e6ea7375ae98dfd24186488d01fdf3b9809b
Ahmed-Mohammed-Badawi <AhmedIsmail73990@azhar.edu.eg>
2024-11-03 14:23:42 +0200

    ADD SOME PLACES TO THE JSON FILE

--- app/[lang]/(main)/clients/page.jsx
+++ app/[lang]/(main)/clients/page.jsx
@@ -1,10 +1,13 @@
-"use client";
+// This is a React component for a Clients page that utilizes PrimeReact's TabView
+// to display two tabs: "Offline Clients" and "Online Clients". The content of each
+// tab is populated by a List component, which takes in the language and client type
+// as props. The direction of the text is determined by the language parameter.
 
+'use client';
 import { TabView, TabPanel } from 'primereact/tabview';
 import List from '../../../components/Main/Clients/List';
 
-
-export default function ClientsPage({params: {lang}}) {
+export default function ClientsPage({ params: { lang } }) {
     return (
         <div className={'card mb-0'} dir={lang === 'en' ? 'ltr' : 'rtl'}>
             <TabView>
